f5c924f2e68a06f51d00e3544c73337f89a1f066
orlinwilliams <orlinwilliams0077@gmail.com>
2023-03-31 10:41:52 -0600

    fix version navigator

--- src/navigator/Navigation.tsx
+++ src/navigator/Navigation.tsx
@@ -1,13 +1,13 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/HomeScreen';
 import { DetailScreen } from '../screens/DetailScreen';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export const Navigation = () => {
     return (
         <Stack.Navigator
-            screenOptions={{ headerShown: false, contentStyle: { backgroundColor: 'white' } }}
+            screenOptions={{ headerShown: false }}
         >
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Detail" component={DetailScreen} />
